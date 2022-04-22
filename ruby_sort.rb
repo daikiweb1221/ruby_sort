8fa82c0a71e655780a7ee85cf5a52b4a15199eef
daikiweb1221 <daiki.web1221@gmail.com>
2022-04-23 06:55:29 +0900

    3回目のコミット

--- ruby_sort.rb
+++ ruby_sort.rb
@@ -1,3 +1,8 @@
+
+#sortメソッドを利用せず、5つの異なる1桁の数字を昇順ソートして出力するプログラムを作成してください。
+
+#アルゴリズム学習
+#バブルソート
 number_array = [3, 5, 9, 7, 1]
 
 #ループ処理で配列の要素同士を比べるため要素の個数を取得
@@ -16,8 +21,40 @@ number_array_size.times do |i|
    end
   end
 end
-
 p number_array
-
 #------------------------------------------------------------
+
+#クイックソート
+number_array = [3, 5, 9, 7, 1]
+
+# クイックソート用のメソッド作成
+def quick_sort(array)
+  # 比べる数値の配列の要素がない場合は比べる必要がないので空配列を返す
+  return array if array.size < 1
+
+  # 数値比較の基準の数値を決める
+  pivot = array.shift
+
+  # 基準の数値より小さい数値(left)と大きい数値(right)を入れるの空配列を定義
+  left = []
+  right = []
+
+  # 数値の要素分ループ処理
+  array.each do |item|
+    # 基準の数値より小さい場合は左に移動(leftの配列に入れる)、大きい場合は右に移動(leftの配列に入れる)
+    if item < pivot
+      left.push(item)
+    else
+      right.push(item)
+    end
+  end
+
+  # leftとrightの配列が昇順になるまでquick_sortメソッド(自分自身のメソッド)を使い、最後に１つの配列にまとめる
+  quick_sort(left) + [pivot] + quick_sort(right)
+end
+
+p quick_sort(number_array)
+
+#---------------------------------------------------------
+
 
